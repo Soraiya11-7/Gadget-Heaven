fd9f5c5477a54966a58422e9ad3ff636faf58523
Soraiya11-7 <soraiyaparvin@gmail.com>
2024-11-03 23:50:32 +0600

    ErrorPage is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement:<ErrorPage></ErrorPage>,
+    element: <div>Hello world!</div>,
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
